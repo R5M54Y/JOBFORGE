955008752353369bc858b7d19c19003b77d589a7
Y3K4G4 <dudaemejing@gmail.com>
2026-09-26 23:48:47 +0700

    fix: move Histats and ad scripts to physical end-of-body position
    Replace Next.js Script components with raw script tags to guarantee
    DOM placement immediately before </body> closing tag.
    
    - Histats bootstrap: raw <script dangerouslySetInnerHTML>
    - ProfitableRateCPM: raw <script src>
    - Preserve exact Histats config (counter 5052094)
    - Preserve exact ad URL
    - Preserve Monetag meta in head

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,31 +31,27 @@ export default function RootLayout({
       </head>
       <body>
         {children}
-        {/* Histats Analytics - Combined bootstrap + dynamic loading (original structure) */}
-        <Script
-          id="histats-combined"
-          strategy="afterInteractive"
+        {/* Histats Analytics - Bootstrap at end of body */}
+        <script
+          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              var _Hasync = _Hasync || [];
-              _Hasync.push(['Histats.start', '1,5052094,4,511,95,18,00000000']);
-              _Hasync.push(['Histats.fasi', '1']);
-              _Hasync.push(['Histats.track_hits', '']);
-              (function() {
-                var hs = document.createElement('script');
-                hs.type = 'text/javascript';
-                hs.async = true;
-                hs.src = ('//s10.histats.com/js15_as.js');
-                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-              })();
+var _Hasync = _Hasync || [];
+_Hasync.push(['Histats.start', '1,5052094,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+  var hs = document.createElement('script');
+  hs.type = 'text/javascript';
+  hs.async = true;
+  hs.src = ('//s10.histats.com/js15_as.js');
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
             `
           }}
         />
-        {/* ProfitableRateCPM Advertising */}
-        <Script
-          src="https://pl31517511.profitableratecpmnetwork.com/0a/6f/45/0a6f45f6cd118b7b19498f9076f1a08f.js"
-          strategy="afterInteractive"
-        />
+        {/* ProfitableRateCPM Advertising - End of body */}
+        <script src="https://pl31517511.profitableratecpmnetwork.com/0a/6f/45/0a6f45f6cd118b7b19498f9076f1a08f.js" />
       </body>
     </html>
   );
